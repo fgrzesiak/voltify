ed8223da2037f857f08c4227cb1e87b5602e5eab
Blqckcore <fj.grzesiak@gmail.com>
2024-12-14 15:28:16 +0100

    add: hot reload

--- apps/Shop/src/main/java/com/example/shop/ShopApplication.java
+++ apps/Shop/src/main/java/com/example/shop/ShopApplication.java
@@ -29,7 +29,7 @@ public class ShopApplication {
     
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-      return String.format("Hello s %s!", name);
+      return String.format("Hello %s!", name);
     }
     
 	 
